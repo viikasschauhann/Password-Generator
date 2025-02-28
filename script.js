0630cec073ea5311cae454268fbcb750bb732c07
viikasschauhann <vchauhan6155@gmail.com>
2025-02-28 11:58:46 +0530

    update changes

--- script.js
+++ script.js
@@ -3,31 +3,38 @@ class Password {
       // alert("welcome to password generator")
       this.pass = ""
     }
-    generatePassword(len) {
-      let chars = "abcdefghijklmnopqrstuvwxyz"
-      let numbers = "1234567890"
-      let special = "!@#$%^&*()"
+    generatePassword(len, includeUpper, includeLower, includeNumbers, includeSpecial) {
+      let chars = "";
+      if(includeUpper) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      if(includeLower) chars += "abcdefghijklmnopqrstuvwxyz";
+      if(includeSpecial) chars += "!@#$%^&*()";
+      if(includeNumbers) chars += "1234567890";
+
+      if (chars === "") {
+        alert("Please select at least one character type");
+        return "";
+    }
       if (len < 3) {
         console.log("Your password should be atleast 3 characters long")
       }
       else {
-        let i = 0
-        while (i < len) {
-          this.pass += chars[Math.floor(Math.random() * chars.length)]
-          this.pass += numbers[Math.floor(Math.random() * numbers.length)]
-          this.pass += special[Math.floor(Math.random() * special.length)]
-          i += 3
-        }
-        this.pass = this.pass.substr(0, len)
+        this.pass = "";
+            for (let i = 0; i < len; i++) {
+                this.pass += chars[Math.floor(Math.random() * chars.length)];
+            }
         return this.pass;
-      }
+       }
     }
   }
   
-  function generatePassword(){
+  function generatePassword() {
     let len = parseInt(document.getElementById("len").value);
+    let includeUpper = document.getElementById("upper").checked;
+    let includeLower = document.getElementById("lower").checked;
+    let includeNumbers = document.getElementById("num").checked;
+    let includeSpecial = document.getElementById("special").checked;
     let p = new Password();
-    let ans = p.generatePassword(len);
+    let ans = p.generatePassword(len, includeUpper, includeLower, includeNumbers, includeSpecial);
     document.querySelector(".result").innerHTML = ans;
-  }
+}
  
